0d359ea7f5d736f524816a09d42c909e378be4c6
manpreetsinghbhullar <mr.manpreetbhullar@gmail.com>
2022-03-22 12:23:08 +0530

    Initial commit
    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,180 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import pic from './images/cake1.jpg'
+
 
 function App() {
+  const [imageUrl, setImageUrl] = useState("");
+  const [postCaption, setPostCaption] = useState("");
+  const [isSharingPost, setIsSharingPost] = useState(false);
+  const [facebookUserAccessToken, setFacebookUserAccessToken] = useState("");
+  /* --------------------------------------------------------
+   *                      FACEBOOK LOGIN
+   * --------------------------------------------------------
+   */
+
+  // Check if the user is authenticated with Facebook
+  useEffect(() => {
+    window.FB.getLoginStatus((response) => {
+      setFacebookUserAccessToken(response.authResponse?.accessToken);
+      console.log(response);
+    });
+  }, []);
+
+  const logInToFB = () => {
+    window.FB.login(
+      (response) => {
+        setFacebookUserAccessToken(response.authResponse?.accessToken);
+      },
+      {
+        // Scopes that allow us to publish content to Instagram
+        scope: "instagram_basic,pages_show_list",
+      }
+    );
+  };
+
+  const logOutOfFB = () => {
+    window.FB.logout(() => {
+      setFacebookUserAccessToken(undefined);
+    });
+  };
+
+  /* --------------------------------------------------------
+   *             INSTAGRAM AND FACEBOOK GRAPH APIs
+   * --------------------------------------------------------
+   */
+
+  const getFacebookPages = () => {
+    return new Promise((resolve) => {
+      window.FB.api(
+        "me/accounts",
+        { access_token: facebookUserAccessToken },
+        (response) => {
+          resolve(response.data);
+        }
+      );
+    });
+  };
+
+  const getInstagramAccountId = (facebookPageId) => {
+    return new Promise((resolve) => {
+      window.FB.api(
+        facebookPageId,
+        {
+          access_token: facebookUserAccessToken,
+          fields: "instagram_business_account",
+        },
+        (response) => {
+          resolve(response.instagram_business_account.id);
+        }
+      );
+    });
+  };
+
+  const createMediaObjectContainer = (instagramAccountId) => {
+    return new Promise((resolve) => {
+      window.FB.api(
+        `${instagramAccountId}/media`,
+        "POST",
+        {
+          access_token: facebookUserAccessToken,
+          image_url: imageUrl,
+          caption: postCaption,
+        },
+        (response) => {
+          resolve(response.id);
+        }
+      );
+    });
+  };
+
+  const publishMediaObjectContainer = (
+    instagramAccountId,
+    mediaObjectContainerId
+  ) => {
+    return new Promise((resolve) => {
+      window.FB.api(
+        `${instagramAccountId}/media_publish`,
+        "POST",
+        {
+          access_token: facebookUserAccessToken,
+          creation_id: mediaObjectContainerId,
+        },
+        (response) => {
+          resolve(response.id);
+        }
+      );
+    });
+  };
+
+  const shareInstagramPost = async () => {
+    setIsSharingPost(true);
+    const facebookPages = await getFacebookPages();
+    const instagramAccountId = await getInstagramAccountId(facebookPages[0].id);
+    const mediaObjectContainerId = await createMediaObjectContainer(
+      instagramAccountId
+    );
+
+    await publishMediaObjectContainer(
+      instagramAccountId,
+      mediaObjectContainerId
+    );
+
+    setIsSharingPost(false);
+
+    // Reset the form state
+    setImageUrl("");
+    setPostCaption("");
+  };
+
+  const onImageChange = event => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+      let pic = URL.createObjectURL(img).slice(5, URL.createObjectURL(img).length);
+      setImageUrl(pic)
+      console.log(pic);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main id="app-main">
+        <section className="app-section">
+          <h3>1. Log in with Facebook</h3>
+          {facebookUserAccessToken ? (
+            <button onClick={logOutOfFB} className="btn action-btn">
+              Log out of Facebook
+            </button>
+          ) : (
+            <button onClick={logInToFB} className="btn action-btn">
+              Login with Facebook
+            </button>
+          )}
+        </section>
+        {facebookUserAccessToken ? (
+          <section className="app-section">
+            <h3>2. Send a post to Instagram</h3>
+            <input
+              value={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
+              placeholder="Enter a JPEG image url..."
+            />
+            <textarea
+              value={postCaption}
+              onChange={(e) => setPostCaption(e.target.value)}
+              placeholder="Write a caption..."
+              rows="10"
+            />
+            <button
+              onClick={shareInstagramPost}
+              className="btn action-btn"
+              disabled={isSharingPost || !imageUrl}
+            >
+              {isSharingPost ? "Sharing..." : "Share"}
+            </button>
+            <input type="file" name="myImage" onChange={onImageChange} />
+          </section>
+        ) : null}
+      </main>
   );
 }
 
